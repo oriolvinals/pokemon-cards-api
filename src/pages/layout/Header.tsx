e562ae68904779e358a992d7007fa6bb2690a456
Oriol <Vials>
2021-03-16 20:12:54 +0100

    Search bar example

--- src/pages/layout/Header.tsx
+++ src/pages/layout/Header.tsx
@@ -1,11 +1,17 @@
-//import React from "react";
-import { IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useState } from "react";
+import { IonHeader, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react";
 
 const Header: React.FC = () => {
+	const [searchText, setSearchText] = useState("");
+
 	return (
 		<IonHeader>
 			<IonToolbar color="primary">
 				<IonTitle>Pokémon</IonTitle>
+				<IonSearchbar
+					value={searchText}
+					onIonChange={(e) => setSearchText(e.detail.value!)}
+				></IonSearchbar>
 			</IonToolbar>
 		</IonHeader>
 	);
