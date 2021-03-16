205c799c55dda3b253d064192fd2aa8402738cf6
Aleix Barcons <aleixbarcons@gmail.com>
2021-03-16 15:52:03 +0100

    Update Header.tsx
    header update

--- src/pages/layout/Header.tsx
+++ src/pages/layout/Header.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+} from "@ionic/react";
+
 const Header: React.FC = () => {
-	return <div> Header </div>;
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar color="red">
+					<IonTitle>Pokémon</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<IonHeader collapse="condense">
+					<IonToolbar color="primary">
+						<IonTitle size="large">Pokémon</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Header;
