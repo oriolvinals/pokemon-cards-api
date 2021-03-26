7475e3e0a81e802a7926ac979d4fc84ccbf3a195
Aleix Barcons <aleixbarcons@gmail.com>
2021-03-26 19:28:10 +0100

    image

--- src/components/Cards/InfoCard.tsx
+++ src/components/Cards/InfoCard.tsx
@@ -9,11 +9,15 @@ interface Info {
 
 const InfoCard = ({ name, supertype, subtypes, hp, type, image }: Info) => {
 	return (
-		<div>
-			{supertype}
-			{subtypes}
-			{hp}
-			<img src={image} alt={name + " card"} />
+		<div className="p-10">
+			<div className="flex mb-2 text-xl justify-between">
+				<p>{supertype} - {subtypes}</p>
+				<div className="flex space-x-2 justify-center items-center">
+					<p>{hp} HP</p>
+					<img src="https://pokemontcg.guru/assets/grass-ec3509d75db6cd146139044107045ccb5bcbb528b02c3de89d709a7be4a0bf90.png" alt="" className="w-7 h-7"/>
+				</div>
+			</div>
+			<img src={image} alt={name + " card"} className="w-80" />
 		</div>
 	);
 };
