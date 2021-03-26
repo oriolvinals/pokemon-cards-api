9d7ad020024a03f3720f0bcaa26969dfb6119811
Aleix Barcons <aleixbarcons@gmail.com>
2021-03-26 18:28:19 +0100

    images

--- src/components/Cards/InfoCard.tsx
+++ src/components/Cards/InfoCard.tsx
@@ -4,11 +4,28 @@ interface Info {
 	subtypes: Array<string>;
 	hp: number;
 	type: Array<string>;
+	image: string;
 	loading: boolean;
 }
 
-const InfoCard = ({name, supertype, subtypes, hp, type, loading}: Info) => {
-	return <div>{supertype} {subtypes}</div>
-}
+const InfoCard = ({
+	name,
+	supertype,
+	subtypes,
+	hp,
+	type,
+	image,
+	loading,
+}: Info) => {
+	if (!loading) return <></>;
+	return (
+		<div>
+			{supertype}
+			{subtypes}
+			{hp}
+			<img src={image} alt={name + " card"} />
+		</div>
+	);
+};
 
 export default InfoCard;
