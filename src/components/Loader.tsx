72e9eded4dd22f4e94a04322a215ff589411eb74
Oriol <Vials>
2021-04-07 14:52:43 +0200

    loading image

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,36 +1,25 @@
+import { card } from "ionicons/icons";
+
 interface Loading {
 	loading: boolean;
 }
 
 const Loader = ({ loading }: Loading) => {
-	if (loading) {
-		return (
-			<div className="fixed z-50 inset-1/4 flex items-center justify-center">
-				<svg
-					className="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-				>
-					<circle
-						className="opacity-25"
-						cx="12"
-						cy="12"
-						r="10"
-						stroke="currentColor"
-						strokeWidth="4"
-					></circle>
-					<path
-						className="opacity-75"
-						fill="currentColor"
-						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-					></path>
-				</svg>
-			</div>
-		);
-	} else {
-		return <></>;
-	}
+	const nLoading = Math.floor(Math.random() * 4 + 1);
+
+	return (
+		<>
+			{loading && (
+				<div className="fixed z-50 inset-1/4 flex items-center justify-center">
+					<img
+						src={"/assets/images/loading" + nLoading + ".svg"}
+						alt="Loader icon"
+						className="animate-spin -ml-1 mr-3 h-20 w-20 text-white"
+					/>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Loader;
