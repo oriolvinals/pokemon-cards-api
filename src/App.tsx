c692d5a9a4228337da532d6477dbaaa3f05fd559
Oriol <Vials>
2021-04-09 21:03:29 +0200

    ionic buid + import tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,10 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
+import "tailwindcss/base.css";
+import "tailwindcss/components.css";
+import "tailwindcss/utilities.css";
+
 /* Theme variables */
 import "./theme/variables.css";
 import LogInPage from "./pages/LogInPage";
